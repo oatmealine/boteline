a520bd381f884ca8d8daddec6b59ab765359d091
jill <krakaka088@gmail.com>
2020-03-11 08:20:47 +0300

    switch to weatherstack

--- src/commands/weather.ts
+++ src/commands/weather.ts
@@ -1,28 +1,28 @@
-import * as weather from 'openweather-apis';
-import * as util from '../lib/util';
 import { MessageEmbed } from 'discord.js';
 import * as CommandSystem from 'cumsystem';
-
-weather.setLang('en');
-weather.setAPPID(process.env.OPENWEATHERAPPID);
-weather.setUnits('metric');
+const got = require('got');
 
 export function addCommands(cs: CommandSystem.System) {
-	cs.addCommand('utilities', new CommandSystem.Command('weather', (msg) => {
-		const params = util.getParams(msg);
-
-		weather.setCity(params.join(' '));
-    
-		weather.getAllWeather((err, res) => {
+	cs.addCommand('utilities', new CommandSystem.Command('weather', (msg, content) => {	
+		got(`http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACKKEY}&query=${encodeURI(content)}`).then((res, err) => {
 			if (err) return msg.channel.send(err);
-			if (!res.sys) return msg.channel.send(`City not found or response empty: \`${JSON.stringify(res)}\``);
+
+			let weather = JSON.parse(res.body);
+
+			if (weather.success === false) return msg.channel.send(`API returned error: \`${weather.error.info}\``);
+
+			let utc = weather.location.utc_offset;
+
+			if (!utc.startsWith('-')) utc = '+' + utc;
 
 			let embed = new MessageEmbed()
-				.setTitle(`${res.name}, ${res.sys.country}`)
-				.setThumbnail('http://openweathermap.org/img/w/' + res.weather[0].icon + '.png')
-				.addField('Weather', res.weather[0].main, true)
-				.addField('Temperature', `${res.main.temp}C`)
-				.addField('Wind speed', `${res.wind.speed}m/s`, true)
+				.setTitle(`${weather.request.query}`)
+				.setDescription(`${weather.location.localtime} (UTC ${utc})`)
+				.setThumbnail(weather.current.weather_icons[0])
+				.addField('Weather description', weather.current.weather_descriptions.join('\n'), true)
+				.addField('Temperature', `${weather.current.temperature}°C\n*Feels like ${weather.current.feelslike}°C*`)
+				.addField('Wind speed', `${weather.current.wind_speed}km/h ${weather.current.wind_dir}`, true)
+				.addField('Humidity', `${weather.current.humidity}%`)
 				.setColor('11FF11');
 
 			return msg.channel.send(embed);
