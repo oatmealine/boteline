f647acb88c610f7158972033cb5ca312974bb0c0
jill <krakaka088@gmail.com>
2020-04-04 00:33:49 +0300

    corona

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,9 @@ import * as parse5 from 'parse5';
 
 import * as winston from 'winston';
 
+import { NovelCovid } from 'novelcovid';
+const track = new NovelCovid();
+
 // modules
 import * as cv from './commands/convert';
 import * as md from './commands/moderation';
@@ -620,6 +623,65 @@ cs.addCommand('utilities', new CommandSystem.Command('mcping', (msg) => {
 			msg.channel.send(err.toString());
 		});
 }));
+
+cs.addCommand('corona', new CommandSystem.SimpleCommand('corona', async (msg, content) => {
+	let stats;
+	if (content) {
+		stats = await track.countries(content);
+		// if (stats.message) stats = await track.states(content);
+		// doesnt work, just errors
+		if (stats.message) {
+			let jhucsse = await track.jhucsse();
+		
+			let jhucsseStats = jhucsse.find((v) => 
+				(
+					(v.province !== null && v.province.toLowerCase().includes(content.toLowerCase())) ||
+					v.country.toLowerCase().includes(content.toLowerCase())
+				) && v.province !== 'Recovered' && v.province !== 'Deaths' && v.province !== 'Cases'
+			);
+
+			if (jhucsseStats) {
+				console.log(jhucsseStats);
+				
+				stats = {
+					cases: jhucsseStats.stats.confirmed,
+					deaths: jhucsseStats.stats.deaths,
+					recovered: jhucsseStats.stats.recovered,
+					country: `${jhucsseStats.country}, ${jhucsseStats.province}`,
+					// @ts-ignore the types are fucked for some reason and its a date
+					updated: Number(Date.parse(jhucsseStats.updatedAt).toPrecision())
+				};
+			} else {
+				stats = await track.all();
+			}
+		}
+	} else {
+		stats = await track.all();
+	}
+
+	console.log(stats);
+
+	let embed = new Discord.MessageEmbed()
+		.setFooter(`Updated at ${util.formatDate(new Date(stats.updated))}`)
+		.setTitle(stats.country || 'Worldwide')
+		.addField('Cases', `${stats.cases} ${stats.todayCases ? `*+${stats.todayCases} today*` : ''} ${stats.deathsPerOneMillion ? `\n${stats.casesPerOneMillion} cases per 1mil` : ''}`, true)
+		.addField('Deaths', `${stats.deaths} ${stats.todayDeaths ? `*+${stats.todayDeaths} today*` : ''} ${stats.deathsPerOneMillion ? `\n${stats.deathsPerOneMillion} deaths per 1mil` : ''}`, true)
+		.addField('Recovered', `${stats.recovered} ${stats.todayRecovered ? `*+${stats.todayRecovered} today*` : ''}`, true)
+		.addField('R/D ratio', `${util.roundNumber(stats.recovered / stats.deaths, 3)}`, true)
+		.setDescription(`Mortality rate: ${util.roundNumber(stats.deaths / stats.cases * 100, 2)}%\nRecovery rate: ${util.roundNumber(stats.recovered / stats.cases * 100, 2)}%`);
+
+	if (stats.active) embed.addField('Active', `${stats.active}${stats.critical ? `, ${stats.critical} critical` : ''}`);
+	if (stats.countryInfo) embed.setThumbnail(stats.countryInfo.flag);
+	if (stats.affectedCountries) embed.addField('Affected countries', stats.affectedCountries, true);
+
+	return embed;
+})
+	.setDescription('corona')
+	.addExample('Russia')
+	.setUsage('[stirng]')
+	.setDisplayUsage('[country/state]')
+	.setUserCooldown(1000)
+	.setGlobalCooldown(700));
 
 cs.addCommand('core', new CommandSystem.Command('latencymeasure', async (msg) => {
 	// testing
