fd43ac1373df490c9314b520f7b4d393d29567a9
jill <krakaka088@gmail.com>
2020-07-15 06:05:41 +0300

    make urban scrollable

--- src/commands/urban.ts
+++ src/commands/urban.ts
@@ -1,34 +1,37 @@
 import * as CommandSystem from 'cumsystem';
 import * as util from '../lib/util';
-import * as urban from 'urban';
 import * as Discord from 'discord.js';
+const got = require('got');
 
 export function addCommands(cs: CommandSystem.System) {
 
-	cs.addCommand(new CommandSystem.Command('urban', msg => {
-		const params = util.getParams(msg);
-		let word = urban(params.join(' '));
-		if (!params[0]) word = urban.random();
+	cs.addCommand(new CommandSystem.Command('urban', async (msg, content) => {
+		let res = await got(`https://api.urbandictionary.com/v0/define?term=${encodeURI(content)}`);
+		let data = JSON.parse(res.body);
+		
+		if (data.list.length === 0) return msg.channel.send('that word doesnt exist!');
 
-		word.first(json => {
-			if (json) {
-				if (json.example === '') {json.example = '(no example given)';}
+		let paginator = new util.Paginator((count) => {
+			let json = data.list[count];
 
-				let embed = new Discord.MessageEmbed()
-					.setTitle(json.word)
-					.setURL(json.permalink)
-				// the written_on thing is like that because of the date format being:
-				// YYYY-mm-ddT00:00:00.000Z
-				// note that the last time is always 0:00
-					.setDescription(`written on ${json.written_on.slice(0, 10)} by ${json.author}\n${json.thumbs_up || '??'} :thumbsup: ${json.thumbs_down || '??'} :thumbsdown:`)
-					.addField('Defintion', util.shortenStr(util.replaceUrbanLinks(json.definition), 1024), true)
-					.addField('Example', util.shortenStr(util.replaceUrbanLinks(json.example), 1024), false);
+			if (json.example === '') {json.example = '(no example given)';}
+	
+			let embed = new Discord.MessageEmbed()
+				.setTitle(json.word)
+				.setURL(json.permalink)
+			// the written_on thing is like that because of the date format being:
+			// YYYY-mm-ddT00:00:00.000Z
+			// note that the last time is always 0:00
+				.setDescription(`written on ${json.written_on.slice(0, 10)} by ${json.author}\n${json.thumbs_up || '??'} :thumbsup: ${json.thumbs_down || '??'} :thumbsdown:`)
+				.addField('Defintion', util.shortenStr(util.replaceUrbanLinks(json.definition), 1024), true)
+				.addField('Example', util.shortenStr(util.replaceUrbanLinks(json.example), 1024), false)
+				.setFooter(`${paginator.count}/${paginator.limit}`);
+	
+			return embed;
+		}, msg.author);
 
-				msg.channel.send('', embed);
-			} else {
-				msg.channel.send('that word doesnt exist!');
-			}
-		});
+		paginator.setLimit(data.list.length);
+		return paginator.start(msg.channel);
 	})
 		.setCategory('utilities')
 		.addAlias('urb')
