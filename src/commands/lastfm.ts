3cace5a07c6ca7755c68410b9a014e66687314f6
jill <krakaka088@gmail.com>
2020-07-15 22:02:45 +0300

    last.fm link system

--- src/commands/lastfm.ts
+++ src/commands/lastfm.ts
@@ -10,21 +10,28 @@ const resultsPerPage = 10;
 
 export function addCommands(cs: CommandSystem.System) {
 	let userAgent = cs.get('userAgent');
+	let userData = cs.get('userData');
 
 	cs.addCommand(new CommandSystem.SimpleCommand('nowplaying', async (msg, content) => {
 		try {
-			let res = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&extended=1&user=${encodeURI(content)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
+			let username = content;
+			if (content.length === 0 && userData[msg.author.id] && userData[msg.author.id].lastfm) {
+				username = userData[msg.author.id].lastfm;
+			}
+			if (username.length === 0) `No account found! Either pass in a username or link your account with ${process.env.PREFIX}lastfm`;
+
+			let res = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&extended=1&user=${encodeURI(username)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
 			let data = JSON.parse(res.body);
 
 			let playingTrack = data.recenttracks.track.find(t => t['@attr'] && t['@attr'].nowplaying === 'true');
 			if (!playingTrack) return 'No track playing';
 
 			// for pfp 
-			let resUser = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${encodeURI(content)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
-			let userData = JSON.parse(resUser.body);
+			let resUser = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${encodeURI(username)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
+			let user = JSON.parse(resUser.body);
 
 			// top tracks provides play amount and rank
-			let resTopTracks = await got(`http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${encodeURI(content)}&limit=100&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
+			let resTopTracks = await got(`http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${encodeURI(username)}&limit=100&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
 			let topTracksData = JSON.parse(resTopTracks.body);
 			let playingTrackExtended = topTracksData.toptracks.track.find(t => t.url === playingTrack.url);
 
@@ -34,7 +41,7 @@ export function addCommands(cs: CommandSystem.System) {
 				.setTimestamp()
 				.setURL(playingTrack.url)
 				.setColor('#63de54')
-				.setAuthor(userData.user.name, userData.user.image.pop()['#text'], userData.user.url);
+				.setAuthor(user.user.name, user.user.image.pop()['#text'], user.user.url);
 
 			let description = '';
 			if (playingTrack.album['#text']) description += `from ${playingTrack.album['#text']}`;
@@ -55,22 +62,28 @@ export function addCommands(cs: CommandSystem.System) {
 		.addAlias('np')
 		.setDescription('check what a user is playing on last.fm')
 		.setGlobalCooldown(1500)
-		.setUsage('(string)')
-		.setDisplayUsage('(username)')
+		.setUsage('[string]')
+		.setDisplayUsage('[username]')
 		.setCategory('last.fm'));
 
 	cs.addCommand(new CommandSystem.Command('recent', async (msg, content) => {
 		try {
-			let res = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=${maxRecentPages * 10}&extended=1&user=${encodeURI(content)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
+			let username = content;
+			if (content.length === 0 && userData[msg.author.id] && userData[msg.author.id].lastfm) {
+				username = userData[msg.author.id].lastfm;
+			}
+			if (username.length === 0) return msg.channel.send(`No account found! Either pass in a username or link your account with ${process.env.PREFIX}lastfm`);
+
+			let res = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=${maxRecentPages * 10}&extended=1&user=${encodeURI(username)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
 			let data = JSON.parse(res.body);
 
 			let playingTrack = data.recenttracks.track.find(t => t['@attr'] && t['@attr'].nowplaying === 'true');
 			let tracks = data.recenttracks.track.filter(t => t !== playingTrack); // filter out current track
 
 			// for pfp 
-			let resUser = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${encodeURI(content)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
-			let userData = JSON.parse(resUser.body);
-			let pfp = userData.user.image.pop()['#text'];
+			let resUser = await got(`http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${encodeURI(username)}&api_key=${process.env.LASTFM_API_KEY}&format=json`, {'user-agent': userAgent});
+			let user = JSON.parse(resUser.body);
+			let pfp = user.user.image.pop()['#text'];
 			let thumbnail;
 			if (playingTrack) thumbnail = playingTrack.image.pop()['#text'];
 
@@ -78,8 +91,8 @@ export function addCommands(cs: CommandSystem.System) {
 				let embed = new Discord.MessageEmbed()
 					.setTitle('Recent scrobbles')
 					.setColor('#63de54')
-					.setURL(userData.user.url)
-					.setAuthor(userData.user.name, pfp, userData.user.url)
+					.setURL(user.user.url)
+					.setAuthor(user.user.name, pfp, user.user.url)
 					.setTimestamp()
 					.addField('Total scrobbles', data.recenttracks['@attr'].total)
 					.setFooter(`${page}/${paginator.limit}`);
@@ -101,7 +114,7 @@ export function addCommands(cs: CommandSystem.System) {
 			}, msg.author);
 
 			paginator.setLimit(Math.ceil(tracks.length / resultsPerPage));
-			paginator.start(msg.channel);
+			return paginator.start(msg.channel);
 		} catch(err) {
 			console.log(err);
 			msg.channel.send(`error: ${err}`);
@@ -109,7 +122,18 @@ export function addCommands(cs: CommandSystem.System) {
 	})
 		.setDescription('check what a user has been listening to on last.fm')
 		.setGlobalCooldown(1500)
+		.setUsage('[string]')
+		.setDisplayUsage('[username]')
+		.setCategory('last.fm'));
+
+	cs.addCommand(new CommandSystem.SimpleCommand('lastfm', (msg, content) => {
+		if (!userData[msg.author.id]) userData[msg.author.id] = {};
+		userData[msg.author.id].lastfm = content;
+		return `Set your last.fm account name to \`${content}\``;
+	})
+		.setDescription('link your last.fm account')
 		.setUsage('(string)')
 		.setDisplayUsage('(username)')
-		.setCategory('last.fm'));
+		.setCategory('last.fm')
+		.addAlias('fm'));
 }
